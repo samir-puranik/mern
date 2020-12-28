7e7f8610cf06ed06a1aca944644f53089996f88d
Samir Puranik <samir.puranik@berkeley.edu>
2020-12-28 02:42:23 -0800

    added GraphQLNonNull and updated README to include playlist link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mern
 MongoDB, Express, ReactJS, Node.js App
+
+# Resources
+- [https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f]()
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,7 @@ const {
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLList,
+    GraphQLNonNull,
     GraphQLInt,
     GraphQLString,
     GraphQLID
@@ -81,8 +82,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args) {
                 let author = new AuthorModel({
@@ -95,9 +96,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: { type: GraphQLID } 
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) } 
             },
             resolve(parent, args) {
                 let book = new BookModel({
